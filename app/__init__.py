0cc8206e64f09d75b9127e3eb104892d80da0321
Weieny <admin@weiney.com>
2019-10-23 09:24:18 +0800

    数据库配置

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_redis import FlaskRedis
 
+from app.models.base import db
+
 redis_store = FlaskRedis()
 
 def create_app():
@@ -12,12 +14,18 @@ def create_app():
     register_extension(app)
     register_blueprint(app)
 
+    with app.app_context():
+        # 在执行create_all之前要保证所有的model至少有一次引入,无法创建
+        db.create_all()
+
+
     return app
 
 
 def register_extension(app):
     Bootstrap(app)
     redis_store.init_app(app, decode_responses=True)
+    db.init_app(app)
 
 def register_blueprint(app):
     from app import web
